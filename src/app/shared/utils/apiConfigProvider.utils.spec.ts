30874f41a1bbd59d3335c0f66d7ff39a8583a3ba
SylwiaKruszynska1 <221315718+SylwiaKruszynska1@users.noreply.github.com>
2026-08-07 15:33:32 +0200

    fix: fix failing test

--- src/app/shared/utils/apiConfigProvider.utils.spec.ts
+++ src/app/shared/utils/apiConfigProvider.utils.spec.ts
@@ -1,22 +1,17 @@
-import { AppStateService, ConfigurationService } from '@onecx/angular-integration-interface'
 import { PortalApiConfiguration } from '@onecx/angular-utils'
 import { environment } from 'src/environments/environment'
 import { Configuration } from '../generated'
 import { apiConfigProvider } from './apiConfigProvider.utils'
 
+jest.mock('@onecx/angular-utils', () => ({
+  PortalApiConfiguration: jest.fn()
+}))
+
 describe('apiConfigProvider', () => {
   it('should create PortalApiConfiguration when provider is called', () => {
-    const configService = {} as ConfigurationService
-    const appStateService = {} as AppStateService
-
     const result = apiConfigProvider()
 
-    expect(PortalApiConfiguration).toHaveBeenCalledWith(
-      Configuration,
-      environment.apiPrefix,
-      configService,
-      appStateService
-    )
+    expect(PortalApiConfiguration).toHaveBeenCalledWith(Configuration, environment.apiPrefix)
     expect(result).toBe((PortalApiConfiguration as jest.Mock).mock.instances[0])
   })
 })
